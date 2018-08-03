6ea8ec9b0304f377881413a25a03ac255c568d95
thelittlewozniak <nathan.pire@gmail.com>
2018-08-04 00:19:02 +0200

    working on translate

--- APIUniTranslate/APIUniTranslate/Controllers/translateController.cs
+++ APIUniTranslate/APIUniTranslate/Controllers/translateController.cs
@@ -11,5 +11,26 @@ namespace APIUniTranslate.Controllers
     [Route("/Translate")]
     public class TranslateController : Controller
     {
+        [HttpGet]
+        public Translate Detect(string q)
+        {
+            IGetAll detectGoogle = new GetAll();
+            Detect data = new Detect();
+            double temp = 0;
+            DetectGoogle detect = detectGoogle.Detect(q);
+            for (int i = 0; i < detect.data.detections.Count; i++)
+            {
+                for (int j = 0; j < detect.data.detections[i].Count; j++)
+                {
+                    if (temp < detect.data.detections[i][j].confidence)
+                    {
+                        temp = detect.data.detections[i][j].confidence;
+                        data.language = detect.data.detections[i][j].language;
+                    }
+                }
+            }
+            return data;
+        }
+
     }
 }
---
+++ APIUniTranslate/APIUniTranslate/Data/Translate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIUniTranslate.Data
+{
+    public class Translate
+    {
+        public string TargetLanguage { get; set; }
+        public string TranslatedText { get; set; }
+    }
+}
