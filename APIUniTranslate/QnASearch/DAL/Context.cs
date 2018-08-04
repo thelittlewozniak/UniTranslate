3d93663d45047f85481764fc1af1567bad23b7b7
Pierre <monamipierrot@live.be>
2018-08-04 18:49:24 +0200

    Fix sql connection string

--- APIUniTranslate/QnASearch/DAL/Context.cs
+++ APIUniTranslate/QnASearch/DAL/Context.cs
@@ -7,9 +7,14 @@ using System.Text;
 
 namespace QnASearch.DAL
 {
-    class Context
+    class Context:DbContext
     {
         public DbSet<QuestionAnswer> QuestionAnswers { get; set; }
         public DbSet<Keyword> Keywords { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=tcp:unitranslate.database.windows.net,1433;Initial Catalog=Interpreters;Persist Security Info=False;User ID=thelittlewozniak;Password=azerty1234@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
     }
 }
