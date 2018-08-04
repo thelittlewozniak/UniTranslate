84e3f575e928eb2860c5069e2c630c8ad8f30368
BuedBued <33833215+BuedBued@users.noreply.github.com>
2018-08-04 19:51:31 +0200

    Ouais j'ai fait l'interface du chatbot (et j'ai écrit en FR en plus hihihi)

--- APIUniTranslate/UI_UniTranslate/wwwroot/js/ChatBot.js
+++ APIUniTranslate/UI_UniTranslate/wwwroot/js/ChatBot.js
@@ -1,20 +1,5 @@
-﻿
-$(document).ready(function () {
-    var me = {};
-
-    var you = {};
-
-    function formatAMPM(date) {
-        var hours = date.getHours();
-        var minutes = date.getMinutes();
-        var ampm = hours >= 12 ? 'PM' : 'AM';
-        hours = hours % 12;
-        hours = hours ? hours : 12; // the hour '0' should be '12'
-        minutes = minutes < 10 ? '0' + minutes : minutes;
-        var strTime = hours + ':' + minutes + ' ' + ampm;
-        return strTime;
-    }
-
+﻿$(document).ready(function () {
+    
     //Detection of the ENTER keypress
     $(".mytext").on("keypress", function (e) {
         if (e.which == 13) {
@@ -25,58 +10,72 @@ $(document).ready(function () {
             }
         }
     });
-
-    //Insert in Chat
-    function insertChat(who, text, time = 0) {
-        var control = "";
-        var date = formatAMPM(new Date());
-
-        if (who == "me") {
-
-            control = '<li style="width:100%">' +
-                '<div class="msj-rta macro">' +
-                '<div class="text text-r">' +
-                '<p>' + text + '</p>' +
-                '<p><small>' + date + '</small></p>' +
-                '</div>' +
-                '</div>' +
-                '</li>';
-        } else {
-            control = '<li style="width:100%;">' +
-                '<div class="msj macro">' +
-                '<div class="text text-l">' +
-                '<p>' + text + '</p>' +
-                '<p><small>' + date + '</small></p>' +
-                '</div>' +
-                '<div class="avatar" style="padding:0px 0px 0px 10px !important"></div>' +
-                '</li>';
-        }
-        setTimeout(
-            function () {
-                $("ul").append(control);
-
-            }, time);
-
-    }
-
-    function resetChat() {
-        $("ul").empty();
-    }
-
-    
 });
 
-//-- Clear Chat
-resetChat();
+var me = {};
 
-//-- Print Messages
-//insertChat("me", "Hello Tom...");
-//insertChat("you", "Hi, Pablo");
-//insertChat("me", "What would you like to talk about today?");
-//insertChat("you", "Tell me a joke");
-//insertChat("me", "Spaceman: Computer! Computer! Do we bring battery?!");
-//insertChat("you", "LOL");
-//insertChat("me", "MES COUILLES AU BORD DE L'EAU");
+var you = {};
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
 
 
-//-- NOTE: No use time on insertChat.
+
+//Insert in Chat
+function insertChat(who, text, time = 0) {
+    var control = "";
+    var date = formatAMPM(new Date());
+
+    if (who == "me") {
+
+        control = '<li style="width:100%">' +
+            '<div class="msj-rta macro">' +
+            '<div class="text text-r">' +
+            '<p>' + text + '</p>' +
+            '<p class=".text-small-r"><small>' + date + '</small></p>' +
+            '</div>' +
+            '</div>' +
+            '</li>';
+    } else {
+        control = '<li style="width:100%;">' +
+            '<div class="msj macro">' +
+            '<div class="text text-l">' +
+            '<p>' + text + '</p>' +
+            '<p class=".text-small-l"><small>' + date + '</small></p>' +
+            '</div>' +
+            '</li>';
+    }
+    setTimeout(
+        function () {
+            $("ul").append(control);
+
+        }, time);
+
+}
+
+
+function testChat() {
+    //-- Clear Chat
+    resetChat();
+
+    //-- Print Messages
+    insertChat("me", "Hello Tom...", 0);
+    insertChat("you", "Hi, Pablo", 500);
+    insertChat("me", "What would you like to talk about today?", 1000);
+    insertChat("you", "Tell me a joke", 1500);
+    insertChat("me", "Spaceman: Computer! Computer! Do we bring battery?!", 2000);
+    insertChat("you", "LOL", 2500);
+    insertChat("me", "MES COUILLES AU BORD DE L'EAU", 3000);
+}
+
+function resetChat() {
+    $("ul").empty();
+}
