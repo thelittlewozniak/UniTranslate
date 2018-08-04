b18671723ee125afff7cb3d97fa9636c20702271
Pierre <monamipierrot@live.be>
2018-08-04 18:22:39 +0200

    My name is Jeff

---
+++ APIUniTranslate/QnASearch/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using QnASearch.POCO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QnASearch.DAL
+{
+    class Context
+    {
+        public DbSet<QuestionAnswer> QuestionAnswers { get; set; }
+        public DbSet<Keyword> Keywords { get; set; }
+    }
+}
